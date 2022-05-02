d183511b79dd3ef389406d684368ac7d0c717ef8
120L020430 <2925218245@qq.com>
2022-05-02 23:17:41 +0800

    1.1 finished（修改）

--- src/P1/MagicSquare.java
+++ src/P1/MagicSquare.java
@@ -3,6 +3,31 @@ package P1;
 import java.io.*;
 
 public class MagicSquare {
+    public static boolean generateMagicSquare(int n) {
+        int magic[][] = new int[n][n];
+        int row = 0, col = n / 2, i, j, square = n * n;
+        for (i = 1; i <= square; i++) {
+            magic[row][col] = i;
+            if (i % n == 0)
+                row++;
+            else {
+                if (row == 0)
+                    row = n - 1;
+                else
+                    row--;
+                if (col == (n - 1))
+                    col = 0;
+                else
+                    col++;
+            }
+        }
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++)
+                System.out.print(magic[i][j] + "\t");
+            System.out.println();
+        }
+        return true;
+    }
     public static boolean isLegalchar(String str) {
         if (str.replaceAll("-*\\.*\\d*\\t*\\n*", "").length() == 0) {
             return true;
@@ -22,34 +47,34 @@ public class MagicSquare {
                 return false;
             }
             String[] digits = squline.split("\t");//split numbers
-            int rows = digits.length;
-            int cols = 0;
-            String[][] Msquare = new String[rows][rows];
-            Msquare[cols++] = digits;
+            int cols = digits.length;
+            int rows = 0;
+            String[][] Msquare = new String[cols][cols];
+            Msquare[rows++] = digits;
             //other lines
             while ((squline = square.readLine()) != null) {
                 if (!isLegalchar(squline)) {
                     return false;
                 } else {
                     digits = squline.split("\t");//split numbers
-                    if (cols >= rows) {
+                    if (rows >= cols) {
                         System.out.println("The number of rows is not equal to cols.");
                         return false;
                     }
-                    if (digits.length != rows) {
-                        System.out.println("The number of rows is not equal to rows.");
+                    if (digits.length != cols) {
+                        System.out.println("The number of cols is not equal to cols.");
                         return false;
                     }
-                    Msquare[cols++] = digits;
+                    Msquare[rows++] = digits;
                 }
             }
             if (cols != rows) {
                 System.out.println("The number of rows in not equal to cols");
                 return false;
             }
-            int[][] MagicSquare = new int[cols][rows];
-            for (int i = 0; i < cols; i++) {
-                for (int j = 0; j < rows; j++) {
+            int[][] MagicSquare = new int[rows][cols];
+            for (int i = 0; i < rows; i++) {
+                for (int j = 0; j < cols; j++) {
                     try {
                         MagicSquare[i][j] = Integer.valueOf(Msquare[i][j]);
                     } catch (NumberFormatException e) {
@@ -62,41 +87,41 @@ public class MagicSquare {
                     }
                 }
             }
-            int first = 0;//sum of the first col
+            int first = 0;//sum of the first row
             int other;//sum of other
-            for (int i = 0; i < rows; i++)
+            for (int i = 0; i < cols; i++)
                 first += MagicSquare[0][i];
-            //judge cols
-            for (int i = 1; i < cols; i++) {
+            //judge rows
+            for (int i = 1; i < rows; i++) {
                 other = 0;
-                for (int j = 0; j < rows; j++) {
+                for (int j = 0; j < cols; j++) {
                     other += MagicSquare[i][j];
                 }
                 if (other != first) {
-                    System.out.println("The sum of col[" + i + "] is not equal to col[0]");
+                    System.out.println("The sum of row[" + i + "] is not equal to row[0]");
                     return false;
                 }
             }
-            //judge rows
-            for (int j = 0; j < rows; j++) {
+            //judge cols
+            for (int j = 0; j < cols; j++) {
                 other = 0;
-                for (int i = 0; i < cols; i++) {
+                for (int i = 0; i < rows; i++) {
                     other += MagicSquare[i][j];
                 }
                 if (other != first) {
-                    System.out.println("The sum of row[" + j + "] is not equal to col[0]");
+                    System.out.println("The sum of col[" + j + "] is not equal to row[0]");
                     return false;
                 }
             }
             //judge diagonals
             other = 0;
             int other1 = 0;
-            for (int i = 0; i < cols; i++) {
+            for (int i = 0; i < rows; i++) {
                 other += MagicSquare[i][i];
-                other1 += MagicSquare[i][cols - i - 1];
+                other1 += MagicSquare[i][rows - i - 1];
             }
             if (other != first | other1 != first) {
-                System.out.println("The sum of diagonal is not equal to col[0]");
+                System.out.println("The sum of diagonal is not equal to row[0]");
                 return false;
             }
             square.close();
